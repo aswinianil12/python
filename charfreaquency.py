cae0682708a26b60dc526964218e217d11a15298
aswinianil12 <aswinianil918@gmail.com>
2024-12-09 21:27:43 -0800

    Update charfreaquency.py

--- charfreaquency.py
+++ charfreaquency.py
@@ -1,38 +1,34 @@
-n=input("Enter a string")
-list_string = n
-newlist = [i for i in list_string.casefold()]
-dict = {}.fromkeys(newlist, 0)
+n=input("Enter a string:")
+list_string=n
+newlist=[i for i in list_string.casefold()]
+dict={}.fromkeys(newlist,0)
 for i in newlist:
     if i in dict:
-        dict[i]+=1
+       dict[i]+=1
 print(list_string)
 print(dict)
-if len(n) >= 3 :
-    if n[-3 :] != "ing":
-        print(n + "ing")
+if len(n) >= 3:
+    if n[-3:] != "ing":
+       print(n + "ing")
     else:
-        print(n + "ly")
+         print(n + "ly")
 else:
     print(n)
-list=["Hello", "World", "programming", "python", "Advanced DS"]
+list=["Hello","world","programming","python","Advanced DS"]
 print(list)
 a=0
 for i in list:
     if len(i)>a:
-        a=len(i)
+      a=len(i)
     else:
         a
 print(a)
-n = 5
-for i in range(n) :
-    for j in range(i) :
-        print('*', end="")
+n=5
+for i in range(n):
+    for j in range(i):
+        print('*',end="")
     print('')
-for i in range(n,0,-1):
-    for i in range(i) :
-    print('*', end="")
-print('')
-
-
-
-
+for i in range( n, 0, -1):
+    for j in range(i):
+        print('*',end="")
+    print('')
