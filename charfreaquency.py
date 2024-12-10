87c81e40d1a2701a19df7e631764bb8a6d7ce5ba
aswinianil12 <aswinianil918@gmail.com>
2024-12-09 20:53:15 -0800

    Update charfreaquency.py

--- charfreaquency.py
+++ charfreaquency.py
@@ -28,7 +28,8 @@ for i in range(n) :
     for j in range(i) :
         print('*', end="")
     print('')
-for i in range(i) :
+for i in range(n,0,-1):
+    for i in range(i) :
     print('*', end="")
 print('')
 
